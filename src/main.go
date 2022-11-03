dd5d6fe9e5e4a740a300e9559da2d6af46747d18
Seongjun Kim <seongjunk@twitter.com>
2022-11-03 13:28:20 -0700

    progress

--- src/main.go
+++ src/main.go
@@ -1,135 +1,49 @@
+// https://github.com/webview/webview/blob/master/examples/bind.go
+// https://stackoverflow.com/questions/66813122/is-it-possible-to-use-a-local-html-file-using-go-and-webview
+
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-	"strings"
-	"time"
-
-	"github.com/charmbracelet/bubbles/list"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	"github.com/webview/webview"
 )
 
-type externalMsg struct{}
+const html = `<button id="increment">Tap me</button>
+<div>You tapped <span id="count">0</span> time(s).</div>
+<script>
+  const [incrementElement, countElement] =
+    document.querySelectorAll("#increment, #count");
+  document.addEventListener("DOMContentLoaded", () => {
+    incrementElement.addEventListener("click", () => {
+      window.increment().then(result => {
+        countElement.textContent = result.count;
+      });
+    });
+  });
+</script>`
 
-func listenForActivity(sub chan struct{}) tea.Cmd {
-	return func() tea.Msg {
-		for {
-			time.Sleep(time.Second * 5)
-			sub <- struct{}{}
-		}
-	}
-}
-
-// A command that waits for the activity on a channel.
-func waitForActivity(sub chan struct{}) tea.Cmd {
-	return func() tea.Msg {
-		return externalMsg(<-sub)
-	}
-}
-
-var docStyle = lipgloss.NewStyle().Margin(1, 2)
-
-type item struct {
-	title string
-	desc  string
-}
-
-func (i item) Title() string       { return i.title }
-func (i item) FilterValue() string { return i.title }
-func (i item) Description() string {
-	desc := strings.Split(i.desc, "/")
-	return strings.Join(desc[2:], "/")
-}
-
-type model struct {
-	sub  chan struct{}
-	list list.Model
-}
-
-func (m model) Init() tea.Cmd {
-	return tea.Batch(
-		listenForActivity(m.sub),
-		waitForActivity(m.sub),
-	)
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c":
-			return m, tea.Quit
-		case "enter":
-			idx := m.list.Index()
-			items := m.list.Items()
-			itemIndexed := items[idx]
-			itemTyped, ok := itemIndexed.(item)
-			if !ok {
-				return m, nil
-			}
-			desc := itemTyped.Description()
-			descSplitted := strings.Split(desc, "/")
-			descLast := descSplitted[len(descSplitted)-1]
-			colonSplitted := strings.Split(descLast, ":")
-			fmt.Println(colonSplitted) // Used this colonSplitted for the line number below
-
-			cmd := exec.Command(
-				"/Applications/GoLand.app/Contents/MacOS/goland",
-				"-l", "147",
-				"/Users/seongjunk/go/src/github.com/bountylabs/service/proxsee_service/watch/watchservice/internal/watch_dynamo/watch_sessions.go")
-			_, err := cmd.Output()
-			if err != nil {
-				return m, tea.Quit
-			}
-		}
-	case tea.WindowSizeMsg:
-		h, v := docStyle.GetFrameSize()
-		m.list.SetSize(msg.Width-h, msg.Height-v)
-	case externalMsg:
-		items := m.list.Items()
-		items = append(items,
-			item{
-				title: "Notice (DynamoDB write - unhandled case)",
-				desc:  "github.com/bountylabs/service/proxsee_service/watch/watchservice/internal/watch_dynamo/watch_sessions.go:143:1"},
-		)
-		m.list.SetItems(items)
-		return m, waitForActivity(m.sub)
-	}
-
-	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
-	return m, cmd
-}
-
-func (m model) View() string {
-	return docStyle.Render(m.list.View())
+type IncrementResult struct {
+	Count uint `json:"count"`
 }
 
 func main() {
-	items := []list.Item{
-		item{
-			title: "Notice (DynamoDB write - unhandled case)",
-			desc:  "github.com/bountylabs/service/proxsee_service/watch/watchservice/internal/watch_dynamo/watch_sessions.go:143:1"},
-		item{
-			title: "Notice (DynamoDB write - unhandled case)",
-			desc:  "github.com/bountylabs/service/proxsee_service/watch/watchservice/internal/watch_dynamo/watch_sessions.go:147:1"},
-		item{
-			title: "Notice (DynamoDB write - unhandled case)",
-			desc:  "github.com/bountylabs/service/proxsee_service/watch/watchservice/internal/watch_dynamo/watch_sessions.go:119:1"},
-	}
+	var count uint = 0
+	w := webview.New(false)
+	defer w.Destroy()
+	w.SetTitle("Bind Example")
+	w.SetSize(480, 320, webview.HintNone)
+	w.Bind("increment", func() IncrementResult {
+		count++
+		return IncrementResult{Count: count}
+	})
 
-	m := model{
-		list: list.New(items, list.NewDefaultDelegate(), 0, 0),
-		sub:  make(chan struct{}),
-	}
-	m.list.Title = "Notices and Warnings"
+	//path, err := os.Getwd()
+	//if err != nil {
+	//	os.Exit(-1)
+	//}
+	//fullPath := "File://" + path + "/front/dist/index.html"
+	//fmt.Println(fullPath)
+	//w.Navigate(fullPath)
 
-	p := tea.NewProgram(m, tea.WithAltScreen())
-	if err := p.Start(); err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
-	}
+	w.SetHtml(html)
+	w.Run()
 }
