60f11017695c399d4107d23ed21f612a04d135e4
seongjunk <seongjunk@twitter.com>
2022-11-07 18:26:45 -0800

    PSVC-10311: add rerun

--- src/front/src/App.svelte
+++ src/front/src/App.svelte
@@ -1,29 +1,37 @@
 <script>
-    import data from './assets/data.json';
+    import data from "./assets/data.json";
 
-    function handleClick(e){
-        console.log("click:", e)
+    function handleClick(e) {
+        console.log("click:", e);
     }
 
     // console.log(data, typeof(data));
     // console.log(data['github.com/bountylabs/service/guest/dynamo'])
     // console.log(typeof(data['github.com/bountylabs/service/guest/dynamo']))
-    Object.entries(data).forEach(([k, v])=> {
-        console.log(k)
-        // console.log(v)
-    })
+    Object.entries(data).forEach(([k, v]) => {
+        console.log(k);
+        const vv = v["pdtv2"];
+        console.log(vv.length);
+        console.log(vv);
+        vv.forEach((vvv) => {
+            console.log("category: ", vvv['category']);
+            console.log("posn: ", vvv['posn']);
+            console.log("msg: ", vvv['message']);
+        });
+    });
+
     // const _data = JSON.parse(data.toString());
     // _data.forEach(d => {
     //     console.log(d)
     // })
-
 </script>
 
 <div class="">
     <h1 class="text-3xl font-bold underline m-2 p-2">Hello!</h1>
-    <button 
+    <button
         class="text-white rounded bg-blue-500 border-2 border-black w-16 hover:bg-blue-600 active:bg-blue-700 m-2 p-2"
-        on:click={handleClick}>
+        on:click={handleClick}
+    >
         Hello!
     </button>
 </div>
